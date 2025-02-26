5405141da7fd3266ed229af4d13fc8d751a5d0e8
kavirlltran <114280928+kavirlltran@users.noreply.github.com>
2025-02-26 09:33:31 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Thêm tính năng tương tác cho nút "Đăng ký ngay"
+document.getElementById('registerBtn').addEventListener('click', function () {
+  alert('Cảm ơn bạn đã quan tâm! Vui lòng liên hệ trung tâm để hoàn tất đăng ký.');
+});
